d10cc0bdb1ef619169ca02382ea654525e63776d
Name <vkonohov19@gmail.com>
2023-06-01 12:36:19 +0300

    update

---
+++ task3/task3.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+while (index <= cube){
+    int number = index*index*index;
+    Console.WriteLine($"{number}");
+    index++;
+}
