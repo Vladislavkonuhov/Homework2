b5d412bd958f8e3bf41d04c6ae006edb6575afac
Name <vkonohov19@gmail.com>
2023-06-02 14:57:13 +0300

    debug

--- task1/task1.cs
+++ task1/task1.cs
@@ -1,8 +1,9 @@
-﻿Console.Write("Введите число: ");
+﻿Console.Clear();
+Console.Write("Введите число: ");
 string number = Console.ReadLine();
 
 void palindr(string number){
-  if (number[0]==number[4] || number[1]==number[3])
+  if (number[0]==number[4] && number[1]==number[3])
     Console.WriteLine("палиндром.");
   else Console.WriteLine("не палиндром.");
 }
